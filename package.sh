814abc606f5078d4d44e1dc2f547dca24e918336
zj <49802283+engun@users.noreply.github.com>
2025-07-17 12:00:18 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -17,9 +17,12 @@ make -f ./Makefile.cross-compiles
 rm -rf ./release/packages
 mkdir -p ./release/packages
 
+# 只保留 Android 平台
 os_all='android'
+# 只保留 ARM 架构
 arch_all='arm arm64'
-extra_all='_ hf'
+# 只保留基本后缀（无额外后缀）
+extra_all='_'
 
 cd ./release
 
@@ -27,44 +30,35 @@ for os in $os_all; do
     for arch in $arch_all; do
         for extra in $extra_all; do
             suffix="${os}_${arch}"
-            if [ "x${extra}" != x"_" ]; then
-                suffix="${os}_${arch}_${extra}"
+            
+            # 特殊处理 ARMv7 架构
+            if [ "$arch" = "arm" ]; then
+                suffix="${os}_arm_v7"
             fi
+            
             frp_dir_name="frp_${frp_version}_${suffix}"
             frp_path="./packages/frp_${frp_version}_${suffix}"
 
-            if [ "x${os}" = x"windows" ]; then
-                if [ ! -f "./frpc_${os}_${arch}.exe" ]; then
-                    continue
-                fi
-                if [ ! -f "./frps_${os}_${arch}.exe" ]; then
-                    continue
-                fi
-                mkdir ${frp_path}
-                mv ./frpc_${os}_${arch}.exe ${frp_path}/frpc.exe
-                mv ./frps_${os}_${arch}.exe ${frp_path}/frps.exe
-            else
-                if [ ! -f "./frpc_${suffix}" ]; then
-                    continue
-                fi
-                if [ ! -f "./frps_${suffix}" ]; then
-                    continue
-                fi
-                mkdir ${frp_path}
-                mv ./frpc_${suffix} ${frp_path}/frpc
-                mv ./frps_${suffix} ${frp_path}/frps
-            fi  
-            cp ../LICENSE ${frp_path}
-            cp -f ../conf/frpc.toml ${frp_path}
-            cp -f ../conf/frps.toml ${frp_path}
+            # Android 属于非 Windows 系统
+            if [ ! -f "./frpc_${suffix}" ]; then
+                echo "frpc_${suffix} not found, skipping"
+                continue
+            fi
+            if [ ! -f "./frps_${suffix}" ]; then
+                echo "frps_${suffix} not found, skipping"
+                continue
+            fi
+            
+            mkdir -p ${frp_path}
+            mv ./frpc_${suffix} ${frp_path}/frpc
+            mv ./frps_${suffix} ${frp_path}/frps
+            cp ../../LICENSE ${frp_path}
+            cp -f ../../conf/frpc.toml ${frp_path}
+            cp -f ../../conf/frps.toml ${frp_path}
 
-            # packages
+            # 打包
             cd ./packages
-            if [ "x${os}" = x"windows" ]; then
-                zip -rq ${frp_dir_name}.zip ${frp_dir_name}
-            else
-                tar -zcf ${frp_dir_name}.tar.gz ${frp_dir_name}
-            fi  
+            tar -zcf ${frp_dir_name}.tar.gz ${frp_dir_name}
             cd ..
             rm -rf ${frp_path}
         done
